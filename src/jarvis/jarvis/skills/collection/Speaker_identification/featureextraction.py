f294629721e6169c45996d0ab0e72502862a1632
Pankaj Kashyap <131644947+Pankajkashyap1@users.noreply.github.com>
2024-07-14 02:56:42 +0530

    Add files via upload

---
+++ src/jarvis/jarvis/skills/collection/Speaker_identification/featureextraction.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sklearn import preprocessing
+import python_speech_features as mfcc
+from python_speech_features import delta
+
+def calculate_delta(array):
+    """Calculate and returns the delta of given feature vector matrix"""
+
+    rows,cols = array.shape
+    deltas = np.zeros((rows,26))
+    N = 2
+    for i in range(rows):
+        index = []
+        j = 1
+        while j <= N:
+            if i-j < 0:
+              first =0
+            else:
+              first = i-j
+            if i+j > rows-1:
+                second = rows-1
+            else:
+                second = i+j 
+            index.append((second,first))
+            j+=1
+        deltas[i] = ( array[index[0][0]]-array[index[0][1]] + (2 * (array[index[1][0]]-array[index[1][1]])) ) / 10
+    return deltas
+
+def extract_features(audio,rate):
+    """extract 20 dim mfcc features from an audio, performs CMS and combines 
+    delta to make it 40 dim feature vector"""    
+    
+    mfcc_feature = mfcc.mfcc(audio,rate, 0.025, 0.01,50,nfft = 1200, appendEnergy = True)    
+    mfcc_feature = preprocessing.scale(mfcc_feature)
+    deltas        = delta(mfcc_feature, 2)
+    double_deltas = delta(deltas, 2)
+    #delta = calculate_delta(mfcc_feature)
+    #combined = np.hstack((mfcc_feature,delta)) 
+    combined      = np.hstack((mfcc_feature, deltas, double_deltas))
+    return combined
